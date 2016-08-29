d4ae5d1110583b15b7963430a5dae49e69eee681
sugaha <gorba1442@gmail.com>
2016-08-29 09:48:54 +0900

    yami

---
+++ src/resource.js
@@ -0,0 +1,38 @@
+var res = {
+  HealTexture_png: "res/Heal_texture.png",
+  DotTexture_png: "res/Dot_texture.png",
+  particle_texture: "res/Dot_texture.png",
+  chara_princessselect_10: "res/char/chara_princessselect_10.png",
+  chara_princessselect_11: "res/char/chara_princessselect_11.png",
+  chara_princessselect_12: "res/char/chara_princessselect_12.png",
+  ss_BattleScene_bg1: "res/field/ss_BattleScene_bg1.jpg",
+  ss_BattleScene_bg2: "res/field/ss_BattleScene_bg2.jpg",
+  ss_BattleScene_bg3: "res/field/ss_BattleScene_bg3.jpg",
+  chara_enemy_1: "res/char/chara_enemy_1.png",
+  chara_enemy_2: "res/char/chara_enemy_2.png",
+  chara_enemy_3: "res/char/chara_enemy_3.png",
+  chara_enemy_4: "res/char/chara_enemy_4.png",
+  chara_enemy_5: "res/char/chara_enemy_5.png",
+  chara_enemy_6: "res/char/chara_enemy_6.png",
+
+  SyusokuTexture2_plist: "res/effect/Syusoku_texture2.plist",
+  SyusokuTexture1_plist: "res/effect/Syusoku_texture4.plist",
+  SyusokuTexture3_plist: "res/effect/Syusoku_texture3.plist",
+  ZangekiTexture1_plist: "res/effect/zangeki_texture1.plist",
+  ZangekiTexture2_plist: "res/effect/zangeki_texture2.plist",
+  FlareTexture1_plist: "res/effect/Flare_texture1.plist",
+  FlareTexture2_plist: "res/effect/Flare_texture2.plist",
+  FlareTexture3_plist: "res/effect/Flare_texture3.plist",
+  FlareTexture4_plist: "res/effect/Flare_texture4.plist",
+
+  YamiTexture1_plist: "res/effect/yami_texture4.plist",
+  YamiTexture2_plist: "res/effect/yami_texture2.plist",
+  YamiTexture3_plist: "res/effect/yami_texture3.plist",
+
+
+};
+
+var g_resources = [];
+for (var i in res) {
+  g_resources.push(res[i]);
+}
